239826f54c43ed3e7811ab66cb14f37fef38dd3c
msa8wsy <msa8wsy@virginia.edu>
2020-08-19 02:20:31 -0400

    Added command line arguments detection

---
+++ python/src/data_collection_script.py
@@ -0,0 +1,7 @@
+import sys
+
+# Gathers the first 4 command line arguments and stores them in variables
+first_name = sys.argv[0]
+last_name = sys.argv[1]
+start_date = sys.argv[2]
+end_date = sys.argv[3]
