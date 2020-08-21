e8f97b605543a3a4403350ec0f89cd1064a3e66b
msa8wsy <msa8wsy@virginia.edu>
2020-08-21 01:22:43 -0400

    More experimentation

--- python/src/collection_script.py
+++ python/src/collection_script.py
@@ -17,3 +17,30 @@ BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=8&day=13&year=2020&game=fd
 dfs = pd.read_html(BASE_URL)
 del dfs[5][2][0]
 print(dfs[5][2])
+print(pd.date_range(start='2/1/2020',end='8/21/2020'))
+
+class Collector:
+    # The url that will be edited to determine the date to be analyzed
+    BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=MONTH&day=DAY&year=YEAR&game=fd"
+
+    def __init__(self, first_name, last_name, start_date, end_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def collect(self):
+        return
+
+    def parse(self, url):
+        return
+
+    # need to edit
+    def next_date(self):
+        return
+
+    def format_name(self):
+        return
+
+    def write_to_csv(self):
+        return
