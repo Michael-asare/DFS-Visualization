74f5df9273ab55ea89b290b52fc5157b2369e0a5
msa8wsy <msa8wsy@virginia.edu>
2020-08-21 02:52:50 -0400

    added trim method, worked on parse method

--- python/src/collection_script.py
+++ python/src/collection_script.py
@@ -3,14 +3,26 @@ import pandas as pd
 import lxml
 import html5lib
 
+URL = "http://rotoguru1.com/cgi-bin/hyday.pl?game=fd&mon=8&day=13&year=2020"
+dfs = pd.read_html(URL)
+print(len(dfs[5][2]))
+
+
+# BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=8&day=13&year=2020&game=fd"
 # dfs = pd.read_html(BASE_URL)
-# del dfs[5][2][0]
 # print(dfs[5][2])
+# print(dfs[5][1])
+# print(dfs[5][1][2] == "Lillard, Damian^")
+# first index = table of interest
+# second index = name (1), fanduel points (2), minutes (7)
 
 
 class Collector:
     # The url that will be edited to determine the date to be analyzed
     BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=MONTH&day=DAY&year=YEAR&game=fd"
+    NAME_ROW = 1
+    FANDUEL_POINTS_ROW = 2
+    MINUTES_ROW = 7
 
     def __init__(self):
         self.first_name = sys.argv[0]
@@ -44,14 +56,29 @@ class Collector:
         return url
 
     def parse(self, url):
+        dfs = pd.read_html(url)
+        if len(dfs) != 7:
+            return
+        for i in range(len(dfs[5][Collector.NAME_ROW])):
+            return
+        name = self.format_name()
         return
 
+    @staticmethod
+    def trim_name(name):
+        if "^" in name:
+            return name[:len(name) - 1]
+        return name
+
     def format_name(self):
-        return
+        last_name = self.last_name.lower().capitalize()
+        first_name = self.first_name.lower().capitalize()
+        name = last_name + ", " + first_name
+        return name
 
     def write_to_csv(self):
         return
 
 
-c = Collector
-c.collect()
+# c = Collector
+# c.collect()
