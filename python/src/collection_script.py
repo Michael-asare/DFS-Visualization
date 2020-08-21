7adb35ff94892f06ee150cedd73404dc8f3daea3
msa8wsy <msa8wsy@virginia.edu>
2020-08-21 16:40:57 -0400

    Commented collection_script.py

--- python/src/collection_script.py
+++ python/src/collection_script.py
@@ -9,10 +9,13 @@ class Collector:
     # The url that will be edited to determine the date to be analyzed
     BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=MONTH&day=DAY&year=YEAR&game=fd"
     DATE_BASE = "mon=MONTH&day=DAY&year=YEAR"
-    # Table of Interest
+    # Table of Interest among located tables
     TOI = 5
+    # Index of name information
     NAME_ROW = 1
+    # Index of fanduel points information
     FANDUEL_POINTS_ROW = 2
+    # Index of minutes information
     MINUTES_ROW = 7
 
     def __init__(self):
@@ -23,6 +26,16 @@ class Collector:
         self.player_data = []
 
     def collect(self):
+        """Collects data for a range of dates for a player and then writes it to a json file
+
+        :return:
+        """
+
+        # For each day
+        # Parse data from a certain date
+        # If data was parsed, add it to the list of player data
+        # Write collected data to a json file
+
         for panda_date in pd.date_range(start=self.start_date, end=self.end_date):
             date = self.format_date(panda_date)
             year = date[0]
@@ -35,6 +48,11 @@ class Collector:
 
     @staticmethod
     def format_date(panda_date):
+        """Formats the pandas api date to a easily indexed list
+
+        :param panda_date: Timestamp
+        :return: list with date information
+        """
         year = panda_date.year
         month = panda_date.month
         day = panda_date.day
@@ -43,6 +61,13 @@ class Collector:
 
     @staticmethod
     def format_url(year, month, day):
+        """Creates a url based off of date data
+
+        :param year: an int that represents the year
+        :param month: an int that represents the month (1-12)
+        :param day: an int that represents the day
+        :return: a string that is a url
+        """
         str_month = "mon=" + str(month)
         str_day = "day=" + str(day)
         str_year = "year=" + str(year)
@@ -50,6 +75,23 @@ class Collector:
         return Collector.BASE_URL.replace(Collector.DATE_BASE, query)
 
     def parse(self, url):
+        """ Given a url, returns fanduel points for the person, if valid. If not valid, returns an empty string.
+
+        :param url: a string that represents a url
+        :return: the amount of fanduel points found for a player at the url. if not valid (NA/DNP/0 minutes) returns
+        an empty string
+        """
+
+        # Tries to
+        # Scan html for a table
+        # If a false table is found, return an empty string
+        # If no tables are found, return an empty string
+        # Otherwise, proceed as normal
+        # Look for the row that has a matching name
+        # Check to see if the player had even played (no 0, NA, DNP for minutes)
+        # If the player had played, return their fanduel points
+        # Else, return an empty string
+
         try:
             dfs = pd.read_html(url)
             if len(dfs[Collector.TOI]) <= 1:
@@ -68,36 +110,64 @@ class Collector:
 
     @staticmethod
     def trim_name(name):
+        """ Given a name (dfs site name), remove the carat if found
+
+        :param name: name of a player, given in the dfs site format
+        :return: trimmed dfs formatted name
+        """
         if "^" in name:
             end = len(name) - 1
             return name[:end]
         return name
 
     def format_name(self):
+        """ Puts name into comparable format
+
+        :return: comparable name (upper case & dfs style)
+        """
         name = self.last_name + ", " + self.first_name
         return name.upper()
 
     def add_to_list(self, date, data):
+        """ Adds player data for a certain date to a list of dicts
+
+        :param date: full format date
+        :param data: fanduel points for a player
+        :return:
+        """
         data_pairing = {
             "date": date,
-            "points": data
+            "points": float(data)
         }
         self.player_data.append(data_pairing)
 
     def write_to_json(self):
+        """ Writes a player's data to a json file
+
+        :return:
+        """
         json_string = json.dumps(self.player_data)
         filename = self.create_filename()
         full_filename = r"..\..\data" + filename
-        print(full_filename)
         with open(full_filename, "w") as file:
             json.dump(self.player_data, file)
 
     def create_filename(self, file_extension=".json"):
+        """ Sets up the filename to be used for writing
+
+        :param file_extension: defaults to json, optionally can be another file extenstion
+        :return:
+        """
         return "\\" + self.last_name.lower().capitalize() + "_" + self.first_name.lower().capitalize() + "_" + \
                Collector.cleanup_date(self.start_date) + "_" + Collector.cleanup_date(self.end_date) + file_extension
 
     @staticmethod
     def cleanup_date(date):
+        """ Turns a date into a format for filenaming
+
+        :param date: Attribute date
+        :return: Attribute date with dashes for slashes
+        """
         return str(date).replace("/", "-")
 
 
