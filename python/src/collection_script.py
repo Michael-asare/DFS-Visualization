c01d5ee1a9c0a42885313d5bb71f06dd4a5d489c
msa8wsy <msa8wsy@virginia.edu>
2020-08-21 01:53:26 -0400

    Coded the format_date & format_url methods

--- python/src/collection_script.py
+++ python/src/collection_script.py
@@ -11,13 +11,12 @@ import html5lib
 
 
 
-# pd.options.display.max_columns = 999
 # BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=MONTH&day=DAY&year=YEAR&game=fd"
 BASE_URL = "http://rotoguru1.com/cgi-bin/hyday.pl?mon=8&day=13&year=2020&game=fd"
 dfs = pd.read_html(BASE_URL)
 del dfs[5][2][0]
 print(dfs[5][2])
-print(pd.date_range(start='2/1/2020',end='8/21/2020'))
+# print(pd.date_range(start='2/1/2020',end='8/21/2020'))
 
 class Collector:
     # The url that will be edited to determine the date to be analyzed
@@ -26,17 +25,37 @@ class Collector:
     def __init__(self, first_name, last_name, start_date, end_date):
         self.first_name = first_name
         self.last_name = last_name
-        self.start_date = start_date
-        self.end_date = end_date
+        # self.start_date = start_date
+        # self.end_date = end_date
+        self.start_date = '1/1/2020'
+        self.end_date = '8/21/2020'
 
     def collect(self):
+        for panda_date in pd.date_range(start=self.start_date, end=self.end_date):
+            date = self.format_date(panda_date)
+            year = date[0]
+            month = date[1]
+            day = date[2]
+            print("The year is: " + str(year) + " The month is: " + str(month) + " The day is: " + str(day))
         return
+
+    @staticmethod
+    def format_date(panda_date):
+        year = panda_date.year
+        month = panda_date.month
+        day = panda_date.day
+        date = [year, month, day]
+        return date
+
+    @staticmethod
+    def format_url(year, month, day):
+        url = BASE_URL
+        url = url.replace(year, "YEAR")
+        url = url.replace(month, "MONTH")
+        url = url.replace(day, "DAY")
+        return url
 
     def parse(self, url):
-        return
-
-    # need to edit
-    def next_date(self):
         return
 
     def format_name(self):
@@ -44,3 +63,7 @@ class Collector:
 
     def write_to_csv(self):
         return
+
+
+c = Collector("Luke", "John", 'xd', 'xd')
+c.collect()
