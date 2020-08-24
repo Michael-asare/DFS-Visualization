a67fb3589317de97a49fc74d18799c7d56aebe18
msa8wsy <msa8wsy@virginia.edu>
2020-08-24 17:13:32 -0400

    Created LZSlashDateFormat Checking

--- java/src/error_checking/date/formats/LeadingZeroSlashDateFormat.java
+++ java/src/error_checking/date/formats/LeadingZeroSlashDateFormat.java
@@ -2,6 +2,8 @@ package error_checking.date.formats;
 
 import error_checking.date.DateFormat;
 
+import java.util.regex.Pattern;
+
 public class LeadingZeroSlashDateFormat extends DateFormat {
     public LeadingZeroSlashDateFormat(String input) {
         this.input = input;
@@ -9,6 +11,16 @@ public class LeadingZeroSlashDateFormat extends DateFormat {
 
     @Override
     public boolean check() {
+        Pattern pattern = Pattern.compile("\\d{2}/\\//\\d{2}/\\//\\d{4}");
+        if(!pattern.matcher(input).matches()) {
+            return false;
+        }
+        String [] ls = input.split("/\\//");
+        int day = Integer.parseInt(ls[0]);
+        int month = Integer.parseInt(ls[1]);
+        int year = Integer.parseInt(ls[2]);
+        if(validDay() && validMonth() && validYear())
+            return true;
         return false;
     }
 }
