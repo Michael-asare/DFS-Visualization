28870417327180b55a8d80d65d7bed5e0121b7ed
msa8wsy <msa8wsy@virginia.edu>
2020-08-24 13:40:03 -0400

    Added controllable variables

--- java/src/IntroductionController.java
+++ java/src/IntroductionController.java
@@ -1,7 +1,28 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 
 public class IntroductionController {
+    @FXML
+    public TitledPane prompt;
+
+    @FXML
+    public Button visualize;
+
+    @FXML
+    public TextField firstName;
+
+    @FXML
+    public TextField lastName;
+
+    @FXML
+    public DatePicker startDate;
+
+    @FXML
+    public DatePicker endDate;
 
     @FXML
     public void handleVisualizePress(ActionEvent actionEvent) {
