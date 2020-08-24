b2aea0bd9888ef376d9e6b98b049e6b5a1baf93b
msa8wsy <msa8wsy@virginia.edu>
2020-08-24 15:34:15 -0400

    Added a custom exception

---
+++ java/src/UnrecognizableInputException.java
@@ -0,0 +1,4 @@
+public class UnrecognizableInputException extends RuntimeException {
+    public UnrecognizableInputException() {}
+    public UnrecognizableInputException(String message) { super(message); }
+}
