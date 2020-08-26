c84a0633d639ac4f194a435dd27d048177b40f15
msa8wsy <msa8wsy@virginia.edu>
2020-08-25 23:54:39 -0400

    Linked controller to screen

--- java/src/VisualizationController.java
+++ java/src/VisualizationController.java
@@ -1,2 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 public class VisualizationController {
+    private String filename;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private ImageView displayImage;
+
+    public VisualizationController(String filename) {
+        this.filename = filename;
+    }
+
+    @FXML
+    public void handleQuitButtonPress(ActionEvent actionEvent) {
+    }
 }
