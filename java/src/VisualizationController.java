08e9fedc7ef7b38c4be974e4cc115cc031d13542
msa8wsy <msa8wsy@virginia.edu>
2020-08-25 23:59:41 -0400

    Added quit button code

--- java/src/VisualizationController.java
+++ java/src/VisualizationController.java
@@ -1,7 +1,13 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VisualizationController {
     private String filename;
@@ -17,6 +23,10 @@ public class VisualizationController {
     }
 
     @FXML
-    public void handleQuitButtonPress(ActionEvent actionEvent) {
+    public void handleQuitButtonPress(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage = (Stage) quitButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/introduction.fxml"));
+        Parent introductionScreenRoot = loader.load();
+        primaryStage.setScene(new Scene(introductionScreenRoot));
     }
 }
