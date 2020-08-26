f8da4735312b8a4ee284fee2800dc8dca468e7d4
msa8wsy <msa8wsy@virginia.edu>
2020-08-26 01:45:30 -0400

    Removed unnecessary constructor

--- java/src/VisualizationController.java
+++ java/src/VisualizationController.java
@@ -18,14 +18,6 @@ public class VisualizationController {
     @FXML
     private ImageView displayImage;
 
-    public VisualizationController() {
-
-    }
-
-    public VisualizationController(String filename) {
-        this.filename = filename;
-    }
-
     public void setFilename(String filename) {
         this.filename = filename;
     }
